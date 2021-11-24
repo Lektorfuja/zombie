ac2ed294c56780f8896be1903fa86eff98835768
Lektorfuja <59904852+Lektorfuja@users.noreply.github.com>
2021-11-24 11:38:30 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,16 @@
+
+
+while True:
+    a=input("attack zombie 1, stop 2")
+    c="you lose"
+    if a=="2":print(c)
+    b=input("attack skeleton 3,stop 4") 
+    e="you lose"
+    if a=="2":print(c)      
+    if a=="1":print(b)
+    if a=="2":print(c)
+    if a=="3":print(d)
+    if a=="4":print(e)
+    d=input("you won")
+   
+    
